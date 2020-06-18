23693e90880de391f79e4ef990288856b0801fd2
Simon Atkins <simonatk@gmail.com>
2020-06-18 18:23:12 +0100

    Add reducer functions and component logic for Storing Result and also Deleting Result

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -13,6 +13,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(storedResult =>
+                      <li onClick={() => this.props.onDeleteResult(storedResult.id)} key={storedResult.id}>{storedResult.value}</li>
+                    )}
+                </ul>
             </div>
         );
     }
@@ -20,7 +27,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     }
 }
 
@@ -29,7 +37,9 @@ const mapDispatchToProps = dispatch => {
         onIncrementCounter: () => dispatch({type: "INCREMENT"}),
         onDecrementCounter: () => dispatch({type: "DECREMENT"}),
         onAddCounter: () => dispatch({type: "ADD", value: 5}),
-        onSubtractCounter: () => dispatch({type: "SUBTRACT", value: 5})
+        onSubtractCounter: () => dispatch({type: "SUBTRACT", value: 5}),
+        onStoreResult: () => dispatch({type: "STORE_RESULT"}),
+        onDeleteResult: (id) => dispatch({type: "DELETE_RESULT", resultElementId: id})
     }
 }
 
