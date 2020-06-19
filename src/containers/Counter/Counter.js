c247c41f1bb0f804c2dbe191c868c64a8dfbcca1
Simon Atkins <simonatk@gmail.com>
2020-06-19 18:59:23 +0100

    Refactor reducer function names into separate action.js file

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import * as actionTypes from '../../store/actions';
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -34,12 +35,12 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        onIncrementCounter: () => dispatch({type: "INCREMENT"}),
-        onDecrementCounter: () => dispatch({type: "DECREMENT"}),
-        onAddCounter: () => dispatch({type: "ADD", value: 5}),
-        onSubtractCounter: () => dispatch({type: "SUBTRACT", value: 5}),
-        onStoreResult: () => dispatch({type: "STORE_RESULT"}),
-        onDeleteResult: (id) => dispatch({type: "DELETE_RESULT", resultElementId: id})
+        onIncrementCounter: () => dispatch({type: actionTypes.INCREMENT}),
+        onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
+        onAddCounter: () => dispatch({type: actionTypes.ADD, value: 5}),
+        onSubtractCounter: () => dispatch({type: actionTypes.SUBTRACT, value: 5}),
+        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, resultElementId: id})
     }
 }
 
